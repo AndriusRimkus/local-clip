7275f2438df42ade06c99e83a4a1411d7ff92ba9
Andrius Rimkus <rimkusrimkus@gmail.com>
2026-01-04 16:49:28 +0200

    Add drag functionality to RangeSlider component using @use-gesture/react

--- src/components/RangeSlider.tsx
+++ src/components/RangeSlider.tsx
@@ -1,5 +1,7 @@
 import { cn } from '@/lib/utils';
 import * as Slider from '@radix-ui/react-slider';
+import { useDrag } from '@use-gesture/react';
+import { useRef, useState } from 'react';
 
 interface RangeSliderProps {
     value: [number, number];
@@ -16,19 +18,68 @@ function RangeSlider({
     max,
     className,
 }: RangeSliderProps) {
+    const trackRef = useRef<HTMLSpanElement>(null!);
+    const rangeRef = useRef<HTMLSpanElement>(null!);
+    const [isDragging, setIsDragging] = useState(false);
+    const initialRangeRef = useRef<[number, number]>([0, 0]);
+
+    const bindDrag = useDrag(
+        ({ movement: [mx], active, first }) => {
+            setIsDragging(active);
+
+            if (first) {
+                initialRangeRef.current = value;
+            }
+
+            const trackWidth = trackRef.current.offsetWidth;
+            const valueRange = max - min;
+            const valueOffset = (mx / trackWidth) * valueRange;
+
+            const rangeDuration =
+                initialRangeRef.current[1] - initialRangeRef.current[0];
+
+            let newStart = initialRangeRef.current[0] + valueOffset;
+            let newEnd = initialRangeRef.current[1] + valueOffset;
+
+            if (newStart < min) {
+                newStart = min;
+                newEnd = min + rangeDuration;
+            } else if (newEnd > max) {
+                newEnd = max;
+                newStart = max - rangeDuration;
+            }
+
+            onValueChange([newStart, newEnd]);
+        },
+        {
+            preventDefault: true,
+        }
+    );
+
     return (
         <Slider.Root
             value={value}
             onValueChange={onValueChange}
             min={min}
             max={max}
+            disabled={isDragging}
             className={cn(
                 'relative flex h-14 select-none touch-none',
                 className
             )}
         >
-            <Slider.Track className="relative bg-gray-50 border-1 border-gray-200 rounded-sm grow h-14">
-                <Slider.Range className="absolute h-full bg-purple-500" />
+            <Slider.Track
+                ref={trackRef}
+                className="relative bg-gray-50 border-1 border-gray-200 rounded-sm grow h-14"
+            >
+                <Slider.Range
+                    ref={rangeRef}
+                    {...bindDrag()}
+                    className={cn(
+                        'absolute h-full bg-purple-500 touch-none',
+                        isDragging ? 'cursor-grabbing' : 'cursor-grab'
+                    )}
+                />
             </Slider.Track>
 
             <Thumb />
